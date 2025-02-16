e3fd1122b52a3af91b53c8d38cd5a360aed41681
HMVE Combrink <44698092+HerkulaasCombrink@users.noreply.github.com>
2025-02-16 10:59:16 +0200

    Update App.py

--- App.py
+++ App.py
@@ -4,8 +4,8 @@ import streamlit as st
 
 if st.button("Test Code"):
   time_series = np.random.randn(100)
-  plt.plot(time_series)
+  st.pyplot(time_series)
   plt.title("Random 100-Unit Time Series")
   plt.xlabel("Time")
   plt.ylabel("Value")
-  st.pyplot(plt.gcf(time_series))
+  
